a2eeadcc9c433a65f6b055bd9fc2e93d72e48224
Burak Şen <burak.sen@tum.de>
2025-07-18 18:12:12 +0200

    Update the link

--- src/config/models.ts
+++ src/config/models.ts
@@ -6,10 +6,10 @@ export const MODEL_CONFIGS: ModelConfig[] = [
     name: "Liner Advection Model v1",
     description: "Basic model",
     modelPath:
-      "/jaxfluids-feed-forward/models/linear_advection_v1/model_slim.onnx",
+      "/jaxfluids-simulation/models/linear_advection_v1/model_slim.onnx",
     datas: [
       {
-        path: "/jaxfluids-feed-forward/models/linear_advection_v1/data.npy",
+        path: "/jaxfluids-simulation/models/linear_advection_v1/data.npy",
         name: "Linear Advection Data",
         yAxisDomain: [0.0, 2.75],
       },
@@ -28,10 +28,10 @@ export const MODEL_CONFIGS: ModelConfig[] = [
     name: "Liner Advection Model v2",
     description: "Basic Linear Advection Model",
     modelPath:
-      "/jaxfluids-feed-forward/models/linear_advection_v2/model_slim.onnx",
+      "/jaxfluids-simulation/models/linear_advection_v2/model_slim.onnx",
     datas: [
       {
-        path: "/jaxfluids-feed-forward/models/linear_advection_v2/data.npy",
+        path: "/jaxfluids-simulation/models/linear_advection_v2/data.npy",
         name: "Linear Advection Data",
         yAxisDomain: [-0.25, 1.25],
       },
@@ -50,30 +50,30 @@ export const MODEL_CONFIGS: ModelConfig[] = [
     name: "Sod Shock Tube WENO5-Z",
     description: "Sod Shock Tube Model",
     modelPath:
-      "/jaxfluids-feed-forward/models/sod_shock_tube/weno5-z/model_slim.onnx",
+      "/jaxfluids-simulation/models/sod_shock_tube/weno5-z/model_slim.onnx",
     datas: [
       {
-        path: "/jaxfluids-feed-forward/models/sod_shock_tube/data/data_v1.npy",
+        path: "/jaxfluids-simulation/models/sod_shock_tube/data/data_v1.npy",
         name: "Sod Shock Tube Data v1",
         yAxisDomain: [-0.25, 1.25],
       },
       {
-        path: "/jaxfluids-feed-forward/models/sod_shock_tube/data/data_v2.npy",
+        path: "/jaxfluids-simulation/models/sod_shock_tube/data/data_v2.npy",
         name: "Sod Shock Tube Data v2",
         yAxisDomain: [-0.5, 2.5],
       },
       {
-        path: "/jaxfluids-feed-forward/models/sod_shock_tube/data/data_v3.npy",
+        path: "/jaxfluids-simulation/models/sod_shock_tube/data/data_v3.npy",
         name: "Sod Shock Tube Data v3",
         yAxisDomain: [-5.0, 105.0],
       },
       {
-        path: "/jaxfluids-feed-forward/models/sod_shock_tube/data/data_v4.npy",
+        path: "/jaxfluids-simulation/models/sod_shock_tube/data/data_v4.npy",
         name: "Sod Shock Tube Data v4",
         yAxisDomain: [-0.25, 1.25],
       },
       {
-        path: "/jaxfluids-feed-forward/models/sod_shock_tube/data/data_v5.npy",
+        path: "/jaxfluids-simulation/models/sod_shock_tube/data/data_v5.npy",
         name: "Sod Shock Tube Data v5",
         yAxisDomain: [-0.25, 1.25],
       },
@@ -92,30 +92,30 @@ export const MODEL_CONFIGS: ModelConfig[] = [
     name: "Sod Shock Tube WENO5-JS",
     description: "Sod Shock Tube Model",
     modelPath:
-      "/jaxfluids-feed-forward/models/sod_shock_tube/weno5-js/model.onnx",
+      "/jaxfluids-simulation/models/sod_shock_tube/weno5-js/model.onnx",
     datas: [
       {
-        path: "/jaxfluids-feed-forward/models/sod_shock_tube/data/data_v1.npy",
+        path: "/jaxfluids-simulation/models/sod_shock_tube/data/data_v1.npy",
         name: "Sod Shock Tube Data v1",
         yAxisDomain: [-0.25, 1.25],
       },
       {
-        path: "/jaxfluids-feed-forward/models/sod_shock_tube/data/data_v2.npy",
+        path: "/jaxfluids-simulation/models/sod_shock_tube/data/data_v2.npy",
         name: "Sod Shock Tube Data v2",
         yAxisDomain: [-0.5, 2.5],
       },
       {
-        path: "/jaxfluids-feed-forward/models/sod_shock_tube/data/data_v3.npy",
+        path: "/jaxfluids-simulation/models/sod_shock_tube/data/data_v3.npy",
         name: "Sod Shock Tube Data v3",
         yAxisDomain: [-5.0, 105.0],
       },
       {
-        path: "/jaxfluids-feed-forward/models/sod_shock_tube/data/data_v4.npy",
+        path: "/jaxfluids-simulation/models/sod_shock_tube/data/data_v4.npy",
         name: "Sod Shock Tube Data v4",
         yAxisDomain: [-0.25, 1.25],
       },
       {
-        path: "/jaxfluids-feed-forward/models/sod_shock_tube/data/data_v5.npy",
+        path: "/jaxfluids-simulation/models/sod_shock_tube/data/data_v5.npy",
         name: "Sod Shock Tube Data v5",
         yAxisDomain: [-0.25, 1.25],
       },
@@ -134,30 +134,30 @@ export const MODEL_CONFIGS: ModelConfig[] = [
     name: "Sod Shock Tube WENO3-JS",
     description: "Sod Shock Tube Model",
     modelPath:
-      "/jaxfluids-feed-forward/models/sod_shock_tube/weno3-js/model.onnx",
+      "/jaxfluids-simulation/models/sod_shock_tube/weno3-js/model.onnx",
     datas: [
       {
-        path: "/jaxfluids-feed-forward/models/sod_shock_tube/data/data_v1.npy",
+        path: "/jaxfluids-simulation/models/sod_shock_tube/data/data_v1.npy",
         name: "Sod Shock Tube Data v1",
         yAxisDomain: [-0.25, 1.25],
       },
       {
-        path: "/jaxfluids-feed-forward/models/sod_shock_tube/data/data_v2.npy",
+        path: "/jaxfluids-simulation/models/sod_shock_tube/data/data_v2.npy",
         name: "Sod Shock Tube Data v2",
         yAxisDomain: [-0.5, 2.5],
       },
       {
-        path: "/jaxfluids-feed-forward/models/sod_shock_tube/data/data_v3.npy",
+        path: "/jaxfluids-simulation/models/sod_shock_tube/data/data_v3.npy",
         name: "Sod Shock Tube Data v3",
         yAxisDomain: [-5.0, 105.0],
       },
       {
-        path: "/jaxfluids-feed-forward/models/sod_shock_tube/data/data_v4.npy",
+        path: "/jaxfluids-simulation/models/sod_shock_tube/data/data_v4.npy",
         name: "Sod Shock Tube Data v4",
         yAxisDomain: [-0.25, 1.25],
       },
       {
-        path: "/jaxfluids-feed-forward/models/sod_shock_tube/data/data_v5.npy",
+        path: "/jaxfluids-simulation/models/sod_shock_tube/data/data_v5.npy",
         name: "Sod Shock Tube Data v5",
         yAxisDomain: [-0.25, 1.25],
       },
